0a15938daf3b18dcb6f10f29fc0c939fc5dbad47
JuKu <kuenzel.justin@t-online.de>
2016-11-29 18:40:11 +0100

    added utils GamePaths.

---
+++ rpg-core/src/main/java/com/jukusoft/rpg/core/path/GamePaths.java
@@ -0,0 +1,12 @@
+package com.jukusoft.rpg.core.path;
+
+/**
+ * Created by Justin on 29.11.2016.
+ */
+public class GamePaths {
+
+    public static String getDataDir () {
+        return "./data/";
+    }
+
+}
