70e3495a65b52c98be8eba05dc655e197e591fbf
JuKu <kuenzel.justin@t-online.de>
2016-11-29 01:49:56 +0100

    added ResourceManager.

---
+++ rpg-game-engine/src/main/java/com/jukusoft/rpg/game/engine/resource/ResourceManager.java
@@ -0,0 +1,21 @@
+package com.jukusoft.rpg.game.engine.resource;
+
+/**
+ * Created by Justin on 29.11.2016.
+ */
+public class ResourceManager {
+
+    /*public static void getImage (String path) {
+        //
+    }*/
+
+    /**
+    * preload image
+     *
+     * @param path path to image
+    */
+    public static void prepareImage (String path) {
+        //
+    }
+
+}
