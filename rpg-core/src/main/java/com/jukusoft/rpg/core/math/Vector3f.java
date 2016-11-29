0d27f1a19d9e82702b3a42f789ed19d0480457cb
JuKu <kuenzel.justin@t-online.de>
2016-11-30 00:19:14 +0100

    added Vector3f to math library.

--- rpg-core/src/main/java/com/jukusoft/rpg/core/math/Vector3f.java
+++ rpg-core/src/main/java/com/jukusoft/rpg/core/math/Vector3f.java
@@ -1,47 +1,44 @@
 package com.jukusoft.rpg.core.math;
 
-import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * class for 2D vector with float values
- *
- * Created by Justin on 21.11.2016.
+ * Created by Justin on 30.11.2016.
  */
-public class Vector2f implements Cloneable, Serializable {
+public class Vector3f {
 
     /**
-    * every float requires 4 bytes
-    */
+     * every float requires 4 bytes
+     */
     private static final int FLOAT_IN_BYTES = 4;
 
     /**
-    * direct (Off Heap) byte buffer to avoid garbage collector cycles
-    */
+     * direct (Off Heap) byte buffer to avoid garbage collector cycles
+     */
     protected volatile ByteBuffer buffer = null;
 
     /**
-    * direct (Off Heap) float buffer to avoid garbage collector cycles
+     * direct (Off Heap) float buffer to avoid garbage collector cycles
      *
      * copy of buffer
-    */
+     */
     protected volatile FloatBuffer floatBuffer = null;
 
     //flag, if vector is readonly
     protected AtomicBoolean readonly = new AtomicBoolean(false);
 
     /**
-    * default constructor
+     * default constructor
      *
      * @param x x coordinate of vector
      * @param y y coordinate of vector
-    */
-    public Vector2f(final float x, final float y) {
-        //allocate new direct (Off Heap) byte buffer with size of 2 bytes
-        this.buffer = ByteBuffer.allocateDirect(FLOAT_IN_BYTES * 2);
+     */
+    public Vector3f(final float x, final float y, final float z) {
+        //allocate new direct (Off Heap) byte buffer with size of 3 bytes
+        this.buffer = ByteBuffer.allocateDirect(FLOAT_IN_BYTES * 3);
 
         //convert to float buffer to access data easely
         this.floatBuffer = this.buffer.asFloatBuffer();
@@ -49,14 +46,15 @@ public class Vector2f implements Cloneable, Serializable {
         //set initial coordinates
         this.floatBuffer.put(0, x);
         this.floatBuffer.put(1, y);
+        this.floatBuffer.put(2, z);
     }
 
     /**
      * default constructor
      */
-    public Vector2f() {
-        //allocate new direct (Off Heap) byte buffer with size of 2 bytes
-        this.buffer = ByteBuffer.allocateDirect(FLOAT_IN_BYTES * 2);
+    public Vector3f() {
+        //allocate new direct (Off Heap) byte buffer with size of 3 bytes
+        this.buffer = ByteBuffer.allocateDirect(FLOAT_IN_BYTES * 3);
 
         //convert to float buffer to access data easely
         this.floatBuffer = this.buffer.asFloatBuffer();
@@ -64,13 +62,14 @@ public class Vector2f implements Cloneable, Serializable {
         //set initial coordinates
         this.floatBuffer.put(0, 0);
         this.floatBuffer.put(1, 0);
+        this.floatBuffer.put(2, 0);
     }
 
     /**
-    * get x coordinate of vector
+     * get x coordinate of vector
      *
      * @return x coordinate
-    */
+     */
     public float getX () {
         return this.floatBuffer.get(0);
     }
@@ -85,10 +84,19 @@ public class Vector2f implements Cloneable, Serializable {
     }
 
     /**
-    * set x coordinate of vector
+     * get z coordinate of vector
+     *
+     * @return z coordinate
+     */
+    public float getZ () {
+        return this.floatBuffer.get(2);
+    }
+
+    /**
+     * set x coordinate of vector
      *
      * @param x x coordinate of vector
-    */
+     */
     public void setX (final float x) {
         if (readonly.get()) {
             throw new IllegalStateException("vector is readonly.");
@@ -111,18 +119,33 @@ public class Vector2f implements Cloneable, Serializable {
     }
 
     /**
-    * set x and y coordinates
+     * set z coordinate of vector
+     *
+     * @param z z coordinate of vector
+     */
+    public void setZ (final float z) {
+        if (readonly.get()) {
+            throw new IllegalStateException("vector is readonly.");
+        }
+
+        this.floatBuffer.put(2, z);
+    }
+
+    /**
+     * set x, y and z coordinates
      *
      * @param x x coordinate of vector
      * @param y y coordinate of vector
-    */
-    public void set (final float x, final float y) {
+     * @param z z coordinate of vector
+     */
+    public void set (final float x, final float y, final float z) {
         if (readonly.get()) {
             throw new IllegalStateException("vector is readonly.");
         }
 
         this.setX(x);
         this.setY(y);
+        this.setZ(z);
     }
 
     /**
@@ -132,93 +155,101 @@ public class Vector2f implements Cloneable, Serializable {
      */
     public float abs () {
         //to set values to stack, get values first (performance optimization)
-        float x = getX();
-        float y = getY();
+        final float x = getX();
+        final float y = getY();
+        final float z = getZ();
 
         //get absolute (betrag) of vector
-        return (float) Math.sqrt((x * x) + (y * y));
+        return (float) Math.sqrt((x * x) + (y * y) + (z * z));
     }
 
     /**
-    * normalize vector
+     * normalize vector
      *
      * @link http://www.conitec.net/manual_d/avec-intro.htm
-    */
+     */
     public void norm () {
         //calculate abs
         float abs = abs();
 
         //normalize values
-        setX(1 / abs * getX()/* / abs*/);
-        setY(1 / abs * getY()/* / abs*/);
+        setX(1 / abs * getX());
+        setY(1 / abs * getY());
+        setZ(1 / abs * getZ());
     }
 
     /**
-    * invert vector
-    */
-    public Vector2f invert () {
+     * invert vector
+     */
+    public Vector3f invert() {
         setX(-getX());
         setY(-getY());
+        setZ(-getZ());
 
         return this;
     }
 
-    public void add (final Vector2f vector) {
+    public void add (final Vector3f vector) {
         this.setX(getX() + vector.getX());
         this.setY(getY() + vector.getY());
+        this.setZ(getZ() + vector.getZ());
     }
 
-    public void add (final float x, final float y) {
+    public void add (final float x, final float y, final float z) {
         this.setX(getX() + x);
         this.setY(getY() + y);
+        this.setZ(getZ() + z);
     }
 
-    public void sub (final Vector2f vector) {
+    public void sub (final Vector3f vector) {
         this.add(vector.invert());
     }
 
-    public void sub (final float x, final float y) {
-        this.add(-x, -y);
+    public void sub (final float x, final float y, final float z) {
+        this.add(-x, -y, -z);
     }
 
-    /*public void mul (Vector2f vector) {
-        //get coordinates, all values are final, so JVM can optimize memory access
-        final float x1 = getX();
-        final float y1 = getY();
-        final float x2 = vector.getX();
-        final float y2 = vector.getY();
-
-        //calculate new x and y values#
-        setX();
-    }*/
-
     /**
-    * scale vector
+     * scale vector
      *
      * @param factor scale factor
-    */
+     */
     public void scale (float factor) {
         setX(getX() * factor);
         setY(getY() * factor);
+        setZ(getZ() * factor);
     }
 
     /**
-     * NOTE: Justin did some shit here.
-    * calculate cross product of 2 vectors
+     * calculate cross product of 3 vectors
      *
      * @param vector vector
      *
      * @return cross product
-    
-    public float crossProduct (final Vector2f vector) {
-        return getX() * vector.getY() - vector.getX() * getY();
-    }*/
+     */
+    public Vector3f crossProduct(final Vector3f vector) {
+        return new Vector3f(
+                getY() * vector.getZ() - getZ() * vector.getY(),
+                getZ() * vector.getX() - getX() * vector.getZ(),
+                getX() * vector.getY() - getY() * vector.getX());
+    }
 
     /**
-    * get direct buffer
+     * calculate the dotproduct (skalarprodukt / punktprodukt) of 3 vectors
+     *
+     * @param vector vector
+     *
+     * @return dot product
+     */
+    public float dotProduct(final Vector3f vector) {
+        return getX() * vector.getX() + getY() * vector.getY() + getZ() * vector.getZ();
+    }
+
+    /**
+     * get direct buffer
      *
      * @return instance of direct buffer
-    */
+     */
     public FloatBuffer getDirectBuffer () {
         return this.floatBuffer.asReadOnlyBuffer();
     }
@@ -233,24 +264,24 @@ public class Vector2f implements Cloneable, Serializable {
     }
 
     /**
-    * copy vector
+     * copy vector
      *
      * @return new vector object with same values
-    */
-    public Vector2f copy () {
+     */
+    public Vector3f copy () {
         //create new vector with same values
-        Vector2f vector = new Vector2f(getX(), getY());
+        Vector3f vector = new Vector3f(getX(), getY(), getZ());
 
         return vector;
     }
 
     @Override
-    public Vector2f clone () {
+    public Vector3f clone () {
         return this.copy();
     }
 
-    public boolean equals (Vector2f vector) {
-        return this.getX() == vector.getX() && this.getY() == vector.getY();
+    public boolean equals (Vector3f vector) {
+        return this.getX() == vector.getX() && this.getY() == vector.getY() && this.getZ() == vector.getZ();
     }
 
     @Override
@@ -275,8 +306,8 @@ public class Vector2f implements Cloneable, Serializable {
     }
 
     /**
-    * cleanUp and release memory
-    */
+     * cleanUp and release memory
+     */
     @SuppressWarnings("all")
     public void release () {
         Field cleanerField = null;
