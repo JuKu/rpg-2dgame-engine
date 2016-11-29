2cb15dc455394db809f79a607dfe95934d7809b7
JuKu <kuenzel.justin@t-online.de>
2016-11-29 22:45:21 +0100

    added class CharacterInfo.

---
+++ rpg-graphic-engine/src/main/java/com/jukusoft/rpg/graphic/opengl/font/CharacterInfo.java
@@ -0,0 +1,26 @@
+package com.jukusoft.rpg.graphic.opengl.font;
+
+/**
+ * Information for character, see LWJGL examples
+ *
+ * Created by Justin on 29.11.2016.
+ */
+public class CharacterInfo {
+
+    protected final int startX;
+    protected final int width;
+
+    public CharacterInfo (final int startX, final int width) {
+        this.startX = startX;
+        this.width = width;
+    }
+
+    public int getStartX () {
+        return this.startX;
+    }
+
+    public int getWidth () {
+        return this.width;
+    }
+
+}
