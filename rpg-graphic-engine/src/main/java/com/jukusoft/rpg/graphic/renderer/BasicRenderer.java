9efa33ad8d084a9353c6035afd8d5e5628ebae32
JuKu <kuenzel.justin@t-online.de>
2016-12-01 01:39:47 +0100

    changed fragment shader.

---
+++ rpg-graphic-engine/src/main/java/com/jukusoft/rpg/graphic/renderer/BasicRenderer.java
@@ -0,0 +1,16 @@
+package com.jukusoft.rpg.graphic.renderer;
+
+/**
+ * Created by Justin on 01.12.2016.
+ */
+public abstract class BasicRenderer implements Renderer {
+
+    public void render () {
+        //
+    }
+
+    public abstract void renderGame ();
+
+    public abstract void renderUI ();
+
+}
---
+++ rpg-graphic-engine/src/main/java/com/jukusoft/rpg/graphic/renderer/Renderer.java
@@ -0,0 +1,12 @@
+package com.jukusoft.rpg.graphic.renderer;
+
+/**
+ * Created by Justin on 01.12.2016.
+ */
+public interface Renderer {
+
+    public void init ();
+
+    public void render ();
+
+}
