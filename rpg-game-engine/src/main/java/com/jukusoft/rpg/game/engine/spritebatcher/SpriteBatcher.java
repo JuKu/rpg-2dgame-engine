90e656db2ddb1332596135714eb20109c39b690c
JuKu <kuenzel.justin@t-online.de>
2016-12-13 19:22:05 +0100

    changed interface SpriteBatcher, added some methods.

--- rpg-game-engine/src/main/java/com/jukusoft/rpg/game/engine/spritebatcher/SpriteBatcher.java
+++ rpg-game-engine/src/main/java/com/jukusoft/rpg/game/engine/spritebatcher/SpriteBatcher.java
@@ -1,5 +1,8 @@
 package com.jukusoft.rpg.game.engine.spritebatcher;
 
+import com.jukusoft.rpg.graphic.animation.BasicAnimation;
+import com.jukusoft.rpg.graphic.opengl.image.OpenGL2DImage;
+
 import java.awt.*;
 
 /**
@@ -16,8 +19,18 @@ public interface SpriteBatcher {
     */
     public void drawText (String text, final float x, final float y, Font font, Color color);
 
+    /**
+     * draw text
+     */
     public void drawText (String text, final float x, final float y, Color color);
 
+    /**
+     * draw text
+     */
     public void drawText (String text, final float x, final float y);
+
+    public void drawAnimation (BasicAnimation animation, final float x, final float y);
+
+    public void drawImage (OpenGL2DImage image, final float x, final float y);
 
 }
