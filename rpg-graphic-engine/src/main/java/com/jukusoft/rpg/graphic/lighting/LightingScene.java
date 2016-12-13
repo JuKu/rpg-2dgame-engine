3a68f03dc2fad6b3933139ada6122481d525aebb
JuKu <kuenzel.justin@t-online.de>
2016-12-13 02:46:38 +0100

    changed LightingScene, added support for point, spot and directional lights.

--- rpg-graphic-engine/src/main/java/com/jukusoft/rpg/graphic/lighting/LightingScene.java
+++ rpg-graphic-engine/src/main/java/com/jukusoft/rpg/graphic/lighting/LightingScene.java
@@ -2,6 +2,9 @@ package com.jukusoft.rpg.graphic.lighting;
 
 import com.jukusoft.rpg.core.math.Vector3f;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Justin on 13.12.2016.
  */
@@ -14,7 +17,26 @@ public class LightingScene {
     /**
     * ambiente light
     */
-    protected Vector3f ambientLight = null;
+    protected Vector3f ambientLight = new Vector3f(0.3f, 0.3f, 0.3f);
+
+    /**
+    * list with all point lights
+    */
+    protected List<PointLight> pointLightList = new ArrayList<>();
+
+    /**
+    * point light array cache
+    */
+    protected PointLight[] pointLightCache = new PointLight[0];
+
+    /**
+    * list with all spot lights
+    */
+    protected List<SpotLight> spotLightList = new ArrayList<>();
+
+    protected SpotLight[] spotLightsCache = new SpotLight[0];
+
+    private DirectionalLight directionalLight = null;
 
     public Vector3f getAmbientLight () {
         return this.ambientLight;
@@ -22,6 +44,52 @@ public class LightingScene {
 
     public void setAmbientLight (Vector3f ambientLight) {
         this.ambientLight = ambientLight;
+    }
+
+    public void addPointLight (PointLight pointLight) {
+        this.pointLightList.add(pointLight);
+        this.pointLightCache = (PointLight[]) this.pointLightList.toArray();
+    }
+
+    public void removePointLight (PointLight pointLight) {
+        this.pointLightList.remove(pointLight);
+        this.pointLightCache = (PointLight[]) this.pointLightList.toArray();
+    }
+
+    public void clearPointLights () {
+        this.pointLightList.clear();
+        this.pointLightCache = new PointLight[0];
+    }
+
+    public PointLight[] getPointLights () {
+        return this.pointLightCache;
+    }
+
+    public void addSpotLight (SpotLight spotLight) {
+        this.spotLightList.add(spotLight);
+        this.spotLightsCache = (SpotLight[]) this.spotLightList.toArray();
+    }
+
+    public void removeSpotLight (SpotLight spotLight) {
+        this.spotLightList.remove(spotLight);
+        this.spotLightsCache = (SpotLight[]) this.spotLightList.toArray();
+    }
+
+    public void clearSpotLights () {
+        this.spotLightList.clear();
+        this.spotLightsCache = new SpotLight[0];
+    }
+
+    public SpotLight[] getSpotLights () {
+        return this.spotLightsCache;
+    }
+
+    public DirectionalLight getDirectionalLight() {
+        return this.directionalLight;
+    }
+
+    public void setDirectionalLight(DirectionalLight directionalLight) {
+        this.directionalLight = directionalLight;
     }
 
 }
