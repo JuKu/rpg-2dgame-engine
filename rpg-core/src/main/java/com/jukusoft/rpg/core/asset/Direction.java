fe345cabca8c60d41114eded8220fae1d12a664c
JuKu <kuenzel.justin@t-online.de>
2016-12-13 19:29:38 +0100

    added enum Direction.

---
+++ rpg-core/src/main/java/com/jukusoft/rpg/core/asset/Direction.java
@@ -0,0 +1,24 @@
+package com.jukusoft.rpg.core.asset;
+
+/**
+ * Created by Justin on 13.12.2016.
+ */
+public enum Direction {
+
+    TOP,
+
+    DOWN,
+
+    LEFT,
+
+    RIGHT,
+
+    TOP_LEFT,
+
+    TOP_RIGHT,
+
+    DOWN_LEFT,
+
+    DOWN_RIGHT
+
+}
